f63a277f96002412633f1e1fb09e4ca9e33ff9f6
Javier Marsicano <javier.marsicano@ix.co>
2021-10-15 12:17:07 -0300

    - cleanup

--- app/src/main/java/com/example/containerapp/MainActivity.kt
+++ app/src/main/java/com/example/containerapp/MainActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import co.ix.vzmve.sdk.MultiViewExperienceSdk
 import co.ix.vzmve.sdk.MultiViewSdkModule
 
@@ -14,27 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        findViewById<Button>(R.id.button_first).setOnClickListener { _ ->
             MultiViewExperienceSdk.init(this, "58283", "NFL")
             MultiViewExperienceSdk.launchMultiViewExperience()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
